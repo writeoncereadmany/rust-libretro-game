7c0b9909be08558286c1b691b545902677e87817
Tom Johnson <tarjohnson1979@gmail.com>
2025-07-22 14:18:36 +0100

    initial commit, using a slightly modified example

---
+++ src/lib.rs
@@ -0,0 +1,189 @@
+use rust_libretro::{
+    contexts::*, core::Core, env_version, input_descriptors, proc::*, retro_core, sys::*, types::*,
+};
+use std::ffi::CString;
+use std::ffi::c_uint;
+
+const WIDTH: c_uint = 320;
+const HEIGHT: c_uint = 240;
+const PIXEL_FORMAT: PixelFormat = PixelFormat::XRGB1555;
+
+const INPUT_DESCRIPTORS: &[retro_input_descriptor] = &input_descriptors!(
+    { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP, "Up" },
+    { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN, "Down" },
+    { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT, "Left" },
+    { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT, "Right" },
+    { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_A, "Action" },
+);
+
+#[derive(CoreOptions)]
+#[categories({
+    "advanced_settings",
+    "Advanced",
+    "Options affecting low-level emulation performance and accuracy."
+},{
+    "not_so_advanced_settings",
+    "Not So Advanced",
+    "Options not affecting low-level emulation performance and accuracy."
+})]
+#[options({
+    "foo_option_1",
+    "Advanced > Speed hack coprocessor X",
+    "Speed hack coprocessor X",
+    "Setting 'Advanced > Speed hack coprocessor X' to 'true' or 'Turbo' provides increased performance at the expense of reduced accuracy",
+    "Setting 'Speed hack coprocessor X' to 'true' or 'Turbo' provides increased performance at the expense of reduced accuracy",
+    "advanced_settings",
+    {
+        { "false" },
+        { "true" },
+        { "unstable", "Turbo (Unstable)" },
+    }
+}, {
+    "foo_option_2",
+    "Simple > Toggle Something",
+    "Toggle Something",
+    "Setting 'Simple > Toggle Something' to 'true' does something.",
+    "Setting 'Toggle Something' to 'true' does something.",
+    "not_so_advanced_settings",
+    {
+        { "false" },
+        { "true" },
+    }
+})]
+struct ExampleCore {
+    option_1: bool,
+    option_2: bool,
+
+    pixels: Vec<u8>,
+    timer: i64,
+    even: bool,
+}
+
+retro_core!(ExampleCore {
+    option_1: false,
+    option_2: true,
+
+    pixels: vec![0; WIDTH as usize * HEIGHT as usize * PIXEL_FORMAT.bit_per_pixel()],
+    timer: 5_000_001,
+    even: true,
+});
+
+impl Core for ExampleCore {
+    fn get_info(&self) -> SystemInfo {
+        SystemInfo {
+            library_name: CString::new("Example Core").unwrap(),
+            library_version: CString::new(env_version!("CARGO_PKG_VERSION").to_string()).unwrap(),
+            valid_extensions: CString::new("").unwrap(),
+
+            need_fullpath: false,
+            block_extract: false,
+        }
+    }
+
+    fn on_set_environment(&mut self, initial: bool, ctx: &mut SetEnvironmentContext) {
+        if !initial {
+            return;
+        }
+
+        ctx.set_support_no_game(true);
+    }
+
+    fn on_init(&mut self, ctx: &mut InitContext) {
+        let gctx: GenericContext = ctx.into();
+        gctx.set_input_descriptors(INPUT_DESCRIPTORS);
+    }
+
+    fn on_get_av_info(&mut self, _ctx: &mut GetAvInfoContext) -> retro_system_av_info {
+        retro_system_av_info {
+            geometry: retro_game_geometry {
+                base_width: WIDTH,
+                base_height: HEIGHT,
+                max_width: WIDTH,
+                max_height: HEIGHT,
+                aspect_ratio: 0.0,
+            },
+            timing: retro_system_timing {
+                fps: 60.0,
+                sample_rate: 0.0,
+            },
+        }
+    }
+
+    fn on_load_game(
+        &mut self,
+        _info: Option<retro_game_info>,
+        ctx: &mut LoadGameContext,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        ctx.set_pixel_format(PIXEL_FORMAT);
+        ctx.set_performance_level(0);
+        ctx.enable_frame_time_callback((1000000.0f64 / 60.0).round() as retro_usec_t);
+
+        let gctx: GenericContext = ctx.into();
+        gctx.enable_audio_callback();
+
+        Ok(())
+    }
+
+    fn on_options_changed(&mut self, ctx: &mut OptionsChangedContext) {
+        match ctx.get_variable("foo_option_1") {
+            Some("true") => self.option_1 = true,
+            Some("false") => self.option_1 = false,
+            _ => (),
+        }
+
+        match ctx.get_variable("foo_option_2") {
+            Some("true") => self.option_2 = true,
+            Some("false") => self.option_2 = false,
+            _ => (),
+        }
+    }
+
+    #[inline]
+    fn on_run(&mut self, ctx: &mut RunContext, delta_us: Option<i64>) {
+        let gctx: GenericContext = ctx.into();
+
+        self.timer += delta_us.unwrap_or(16_666);
+
+        let input = unsafe { ctx.get_joypad_bitmask(0, 0) };
+
+        if input.contains(JoypadState::START) && input.contains(JoypadState::SELECT) {
+            return gctx.shutdown();
+        }
+
+        if !ctx.can_dupe() || self.timer >= 1_000_000 || input.contains(JoypadState::A) {
+            self.timer = 0;
+            self.even = !self.even;
+
+            let width = WIDTH;
+            let height = HEIGHT;
+
+            let color_a = if self.even { color_xrgb1555(0xff, 0xbb, 0) } else { 0 };
+            let color_b = if !self.even { color_xrgb1555(0xff, 0xbb, 0) } else { 0 };
+
+            for (i, chunk) in self.pixels.chunks_exact_mut(PIXEL_FORMAT.bit_per_pixel()).enumerate() {
+                let x = (i % width as usize) as f64 / width as f64;
+                let y = (i / width as usize) as f64 / height as f64;
+
+                let total = (16f64 * x).floor() + (16f64 * y).floor();
+                let even = total as usize % 2 == 0;
+
+                let color = if even { color_a } else { color_b };
+
+                chunk[0] = color as u8;
+                chunk[1] = (color >> 8) as u8;
+            }
+
+            ctx.draw_frame(self.pixels.as_ref(), width, height, width as usize * PIXEL_FORMAT.bit_per_pixel());
+        } else if ctx.can_dupe() {
+            ctx.dupe_frame();
+        }
+    }
+
+    fn on_write_audio(&mut self, ctx: &mut AudioContext) {
+        ctx.queue_audio_sample(0, 0);
+    }
+}
+
+fn color_xrgb1555(red: u8, green: u8, blue: u8) -> u16 {
+    (((red >> 3) as u16) << 10) + (((green >> 3) as u16) << 5) + (blue >> 3) as u16
+}
