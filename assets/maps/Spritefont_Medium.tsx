45b4b22b9574d00814ac1848233f080d0c8ce7ba
Tom Johnson <tarjohnson1979@gmail.com>
2025-08-18 15:19:22 +0300

    add tilesheets for fonts and sprites

---
+++ assets/maps/Spritefont_Medium.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Spritefont_Medium" tilewidth="8" tileheight="8" tilecount="70" columns="10">
+ <image source="../fonts/spritefont.png" width="80" height="56"/>
+</tileset>
---
+++ assets/maps/Spritefont_Small.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Spritefont_Small" tilewidth="4" tileheight="6" tilecount="70" columns="10">
+ <image source="../fonts/spritefont_small.png" width="40" height="42"/>
+</tileset>
