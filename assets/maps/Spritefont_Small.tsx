2c3a15e63ff542a868814f9dce7f1d07abd88ac6
Tom Johnson <tarjohnson1979@gmail.com>
2025-08-19 13:21:58 +0300

    add font class to fonts

--- assets/maps/Spritefont_Small.tsx
+++ assets/maps/Spritefont_Small.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Spritefont_Small" tilewidth="4" tileheight="6" tilecount="70" columns="10">
+<tileset version="1.10" tiledversion="1.11.0" name="Spritefont_Small" class="Font" tilewidth="4" tileheight="6" tilecount="70" columns="10">
  <image source="../fonts/spritefont_small.png" width="40" height="42"/>
  <tile id="0">
   <properties>
