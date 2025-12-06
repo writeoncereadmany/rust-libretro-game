31633bb387c78160add638608c2e10e9065678c8
Tom Johnson <tarjohnson1979@gmail.com>
2025-12-06 13:28:47 +0000

    extract spawner method for less nesting

--- src/entities/coin.rs
+++ src/entities/coin.rs
@@ -1,27 +1,32 @@
-use derive::Event;
-use engine::entities::entity::entity;
-use engine::events::dispatcher::Dispatcher;
-use engine::events::spawner::Spawner;
 use crate::assets::map::Spawn;
 use crate::component::graphics::{Animation, Phase, Sprite};
 use crate::component::physics::Position;
+use derive::Event;
+use engine::entities::entity::{Entities, entity};
+use engine::events::dispatcher::Dispatcher;
+use engine::events::event::Events;
+use engine::events::spawner::Spawner;
 
 #[derive(Event)]
 pub struct SpawnCoin(f64, f64);
 
 pub fn register(dispatcher: &mut Dispatcher, spawner: &mut Spawner<Spawn>) {
-    dispatcher.register(|&SpawnCoin(x, y), world, events| {
-        world.spawn(entity()
+    dispatcher.register(spawn_coin);
+
+    spawner.register("Coin", |spawn, events| {
+        events.fire(SpawnCoin(spawn.x as f64, spawn.y as f64))
+    });
+}
+
+fn spawn_coin(&SpawnCoin(x, y): &SpawnCoin, world: &mut Entities, events: &mut Events) {
+    world.spawn(
+        entity()
             .with(Animation {
                 sprites: vec!["coin_1", "coin_2", "coin_3", "coin_4"],
                 period: 0.5,
             })
-            .with(Phase((-0.005*x + 0.015*y) % 1.0))
+            .with(Phase((-0.005 * x + 0.015 * y) % 1.0))
             .with(Sprite("coin_1"))
-            .with(Position(x, y)));
-    });
-
-    spawner.register("Coin", |spawn, events|
-        events.fire(SpawnCoin(spawn.x as f64, spawn.y as f64))
+            .with(Position(x, y)),
     );
 }
