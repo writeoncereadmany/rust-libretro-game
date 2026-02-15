d263d1ad339a234b6a375adab01b0f526a6b5c34
Tom Johnson <tarjohnson1979@gmail.com>
2026-02-15 13:33:05 +0000

    run in fullscreen regardless of retroarch default

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-/Applications/RetroArch.app/Contents/MacOS/RetroArch -L target/release/libpandamonium.dylib pandamonium.tar
+/Applications/RetroArch.app/Contents/MacOS/RetroArch -f -L target/release/libpandamonium.dylib pandamonium.tar
