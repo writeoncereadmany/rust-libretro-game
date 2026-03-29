e7397034e5cac30a7e2de9e08d781c342988a7e7
tomjohnson <tarjohnson1979@gmail.com>
2026-03-29 18:57:35 +0100

    add radial zoom in for flag

--- src/entities/flag.rs
+++ src/entities/flag.rs
@@ -1,3 +1,4 @@
+use std::time::Duration;
 use crate::component::collisions::{Collided, Pickup};
 use crate::component::graphics::{Animation, Sprite};
 use crate::component::physics::Position;
@@ -10,9 +11,13 @@ use engine::events::event::Events;
 use engine::events::spawner::{Spawn, Spawner};
 use engine::shapes::shape::Shape;
 use tiled::PropertyValue;
+use crate::entities::radial::SpawnRadials;
 
 #[derive(Event)]
 pub struct SpawnFlag(f64, f64, String);
+
+#[derive(Event)]
+pub struct SpawnFlagpole(f64, f64);
 
 #[derive(Event)]
 pub struct PickupFlag(EntityId);
@@ -25,11 +30,14 @@ pub struct NextLevel(String);
 
 pub fn register(dispatcher: &mut Dispatcher, spawner: &mut Spawner) {
     dispatcher.register(spawn_flag);
+    dispatcher.register(spawn_flagpole);
     dispatcher.register(pickup_flag);
     dispatcher.register(collect_flag);
 
     spawner.register("Flag", |spawn, events| {
-        events.fire(SpawnFlag(spawn.x, spawn.y, destination(&spawn).unwrap_or("start".to_string())))
+        events.fire(SpawnRadials(spawn.x, spawn.y, vec!["ball_blue"], 8));
+        events.schedule(Duration::from_secs_f64(2.4), SpawnFlag(spawn.x, spawn.y, destination(&spawn).unwrap_or("start".to_string())));
+        events.fire(SpawnFlagpole(spawn.x, spawn.y,));
     });
 }
 
@@ -61,14 +69,15 @@ fn spawn_flag(SpawnFlag(x, y, dest): &SpawnFlag, world: &mut Entities, _events: 
             .with(NextLevel(dest.clone()))
             .with(Shape::bbox(0.0, 0.0, 12.0, 12.0))
     );
+}
 
+fn spawn_flagpole(SpawnFlagpole(x, y): &SpawnFlagpole, world: &mut Entities, _events: &mut Events) {
     world.spawn(
         entity()
             .with(Sprite::sprite("flagpole_silver", 3))
             .with(Position(*x, *y))
     );
 }
-
 
 fn pickup_flag(Collided(first, second): &Collided, world: &mut Entities, events: &mut Events) {
     world.apply_to(first, |Flag()| events.fire(PickupFlag(*first)));
