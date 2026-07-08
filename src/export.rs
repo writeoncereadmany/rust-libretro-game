13242af7fe20a95231e466c8cc5625ceed836e10
Tom Johnson <tarjohnson1979@gmail.com>
2026-07-08 10:14:16 +0300

    use moved export assets function

--- src/export.rs
+++ src/export.rs
@@ -4,13 +4,5 @@ use std::io::Write;
 use engine::assets::Assets;
 
 pub fn main() -> std::io::Result<()> {
-    let args = env::args().collect::<Vec<String>>();
-    let mut file_output = File::create(args.get(2).expect("No output file specified"))?;
-
-    let mut assets = Assets::new();
-    assets.load_from_filesystem("assets");
-
-    let binary_data = serde_json::to_vec(&assets)?;
-    file_output.write(binary_data.as_slice())?;
-    file_output.flush()
+    engine::export::export_assets()
 }
